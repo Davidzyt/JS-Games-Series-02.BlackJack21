33c77583be7c24d4104ced0643b6fb622e56d32b
Davidzyt <yuntian.zou@hotmail.com>
2021-04-30 16:41:40 +0200

    change array to values& add random picking up function

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-//Creat an array{3,4,5,6,7,8,9,10,J,Q,K,A};
-const cards=["3","4","5","6","7","8","9","10","J","Q","K","A",
-             "3","4","5","6","7","8","9","10","J","Q","K","A",
-             "3","4","5","6","7","8","9","10","J","Q","K","A",
-             "3","4","5","6","7","8","9","10","J","Q","K","A"];
+//Creat an array{2,3,4,5,6,7,8,9,10,J,Q,K,A};
+const cardsValues=["2","3","4","5","6","7","8","9","10","11"];
 
-//Give value to each element{3=3,4=4,5=5,6=6,7=7,8=8,9=9,10,J,Q,K=10,A=1 or A=11}
+//Definite variable
+const playerHandElementHTML=document.getElementById("playerHand");
+const dealerHandElementHTML=document.getElementById("dealerHand");
+
 //Random pick up two elements from the array and give them to the player;
+//Show the values separately,
+//Save the values and add them in total scores;
+
+let playerScoresInTotal=0;
+let dealerScoresInTotal=0;
+
+const randomChoice = () => {
+    const randomIndex = Math.floor(Math.random() * cardsValues.length);
+    return cardsValues[randomIndex];
+};
+playerScoresInTotal+=randomChoice();
+
+playerScoresInTotal+=randomChoice();
+
+dealerScoresInTotal+=randomChoice();
+
+dealerScoresInTotal+=randomChoice();
+
+
+
+
+const displayValue=()=>{
+            playerHandElementHTML.innerHTML=randomChoice();
+    }
+
+    displayValue();
+
 //Random pick up two elements from the array and give them to the computer;
 //Calculate player's scores(If one element's value is lower than 10 or equal to 10, A's value=11;otherwise A's value=1)
 //Display the scores on the screen;
 //Calculate computer's scores
 //If player's score or computer's score is 21, the player wins/the computer wins.
-let playerScoresInTotal=0;
-let dealerScoresInTotal=0;
