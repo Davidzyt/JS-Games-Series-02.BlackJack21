6eaf5166e868ff659e312420e0b59097c0131f86
Davidzyt <yuntian.zou@hotmail.com>
2021-04-30 14:52:53 +0200

    change some buttons in html

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 //Creat an array{3,4,5,6,7,8,9,10,J,Q,K,A};
-const cards=["3","4","5","6","7","8","9","10","J","Q","K","A"];
+const cards=["3","4","5","6","7","8","9","10","J","Q","K","A",
+             "3","4","5","6","7","8","9","10","J","Q","K","A",
+             "3","4","5","6","7","8","9","10","J","Q","K","A",
+             "3","4","5","6","7","8","9","10","J","Q","K","A"];
+
 //Give value to each element{3=3,4=4,5=5,6=6,7=7,8=8,9=9,10,J,Q,K=10,A=1 or A=11}
 //Random pick up two elements from the array and give them to the player;
 //Random pick up two elements from the array and give them to the computer;
-//Calculate player's scores(If one element's value is lower than 10, and the other element is A=>A's value=11)
+//Calculate player's scores(If one element's value is lower than 10 or equal to 10, A's value=11;otherwise A's value=1)
 //Display the scores on the screen;
 //Calculate computer's scores
-//If player's score
+//If player's score or computer's score is 21, the player wins/the computer wins.
